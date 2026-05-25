f91421a5df8fe834007b7b03f6d5eb42b1b5f677
Wisulve-328 <440042792@gapps.yrdsb.ca>
2026-05-25 12:05:48 -0400

    The animation group for the aircraft has been set up

--- Player.java
+++ Player.java
@@ -14,16 +14,23 @@ public class Player extends Actor
     public Player(){
         GreenfootImage image = getImage();
         image.scale(50, 50);
+        //make animation 
         thrusterAnimation = new GreenfootImage[2];
-        thrusterAnimation[0] = new GreenfootImage("ship_fire1.png");
-        thrusterAnimation[1] = new GreenfootImage("ship_fire2.png");
+        thrusterAnimation[0] = new GreenfootImage("warcraft.png");
+        thrusterAnimation[0].scale(50, 50);
+        thrusterAnimation[1] = new GreenfootImage("warcraft2.png");
+        thrusterAnimation[1].scale(50, 50);
         setImage(thrusterAnimation[0]);
     }
     
     public void act()
     {
         checkKeyPress();
-        
+        count++;
+        if(count % 10 == 0){
+            animationIndex = (animationIndex + 1) % thrusterAnimation.length;
+            setImage(thrusterAnimation[animationIndex]);
+        }
     }
     private void checkKeyPress(){
         if(Greenfoot.isKeyDown("left")){
